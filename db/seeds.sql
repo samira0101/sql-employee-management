2623ebd4f8a9784d05e850a22a2add541dbd6953
Allure <samira-h@hotmail.co.uk>
2022-03-19 22:48:44 +0000

    creating role informations

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,3 +9,12 @@ VALUES
 ('IT'),
 ('Sales');
 
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Accountant', 40000, 1),
+('Manager', 60000, 2),
+('Paralegal', 30000, 3),
+('Engineer', 50000, 4),
+('Programmer', 80000, 5),
+('Sales Rep', 20000, 6);
+
