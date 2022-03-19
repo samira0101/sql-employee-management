c73c21d52f3fd637241cf41a543481b685585588
Allure <samira-h@hotmail.co.uk>
2022-03-19 22:51:08 +0000

    creating and connecting database/sql2

--- app.js
+++ app.js
@@ -3,3 +3,17 @@ const mysql2 = require('mysql2');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table')
 
+// Creating database connection
+const connection = mysql2.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'employer_managementDB'
+})
+
+// connects to sql2 server and sql2 database
+connection.connect(function(err){
+    if (err) throw err;
+    options();
+})
